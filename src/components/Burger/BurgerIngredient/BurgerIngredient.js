f996621bca705366e61eab5fae47f7e6f4a12b55
vietduc030496 <nguyenvietduc030496@gmail.com>
2021-04-13 00:30:00 +0700

    init commit

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingredient = <div className='BreadBottom'></div>;
+            break;
+        case ('bread-top'):
+            ingredient = (
+                <div className='BreadTop'>
+                    <div className='Seeds1'></div>
+                    <div className='Seeds2'></div>
+                </div>
+            );
+            break;
+        case ('cheese'):
+            ingredient = <div className='Cheese'></div>;
+            break;
+        case ('salad'):
+            ingredient = <div className='Salad'></div>;
+            break;
+        case ('bacon'):
+            ingredient = <div className='Salad'></div>;
+            break;
+        default:
+            ingredient = null;
+    }
+
+    return ingredient;
+};
+
+export default burgerIngredient;
